8ac374fcd8bc801e52975e542088b6852d4a72eb
Kuppusamy-R <Kuppusamy-R@users.noreply.github.com>
2020-02-20 19:46:52 +0530

    works fine with outputs

--- MorePizza.java
+++ MorePizza.java
@@ -5,12 +5,17 @@ public class MorePizza {
         Pizza previousOrderChoice;
         int minSliceCount;
         int maxSliceCount;
-        int Slice[];
+        List<Integer> Slice;
     
-        Pizza(int minSliceCount,int maxSliceCount,int[] Slice){
+        Pizza(int minSliceCount,int maxSliceCount,List<Integer> Slice){
             this.minSliceCount = minSliceCount;
             this.maxSliceCount = maxSliceCount;
-            this.Slice = Slice;
+            this.Slice = new ArrayList<Integer>();
+            System.out.println(Slice.toString()+"income");
+            for(int i: Slice){
+                this.Slice.add(i);
+            }
+            System.out.println(this.Slice.toString()+"constructor");
     
         }
     }
@@ -27,15 +32,17 @@ public class MorePizza {
     public PizzaCombination selectedPizzaCombination;
     public static int minVal;
     public static int maxVal;
+    public static List<Integer> combinationIndex;
     public void addPizzaCombination(List<Integer> combination){
         PizzaCombination newPizzaCombination = new PizzaCombination(combination);
         newPizzaCombination.next = selectedPizzaCombination;
         selectedPizzaCombination = newPizzaCombination;
     }
 
-    public void addPizza(int minSliceCount, int maxSliceCount, int[] Slice) 
+    public void addPizza(int minSliceCount, int maxSliceCount, List<Integer> Slice) 
     { 
-        Pizza newPizza = new Pizza(minSliceCount, maxSliceCount, Slice); 
+        System.out.println(Slice.toString()+" add pizza");
+        Pizza newPizza = new Pizza(minSliceCount, maxSliceCount,Slice); 
         newPizza.previousOrderChoice = selectedPizza; 
         selectedPizza = newPizza; 
     }
@@ -52,40 +59,76 @@ public class MorePizza {
         while (tempPizza != null) 
         { 
             System.out.println("Min: "+tempPizza.minSliceCount); 
+            System.out.println("Max: "+tempPizza.maxSliceCount);
+            System.out.println("Slice: "+tempPizza.Slice.toString());
             tempPizza = tempPizza.previousOrderChoice;
         } 
     }
     public static void main(String[] args) {
         MorePizza tempPizza= new MorePizza();
-        int pizza[] = {2,5,6,7,8,9,0};
-        tempPizza.addPizza(1,2,pizza);
-        tempPizza.addPizza(2,2,pizza);
-        tempPizza.printList();
-        System.out.println();
         
-        int maxSlice = 17;
-        int pizzaArray[] = {2,5,6,7,8,9,0};
-		int pizzaToBuy = 3;
-		
-        int i;
+        //int maxSlice = 17;
+        //int pizzaArray[] = {2,5,6,8};
+
+        int maxSlice = 100;
+        int pizzaArray[] = {4, 14, 15, 18, 29, 32, 36, 82, 95, 95};
+        int max = 3;
+        for(int pizzaToBuy = 1; pizzaToBuy <= max; pizzaToBuy++){
+            int i;
         for(i = 0; i<pizzaArray.length; i++){		    
                 List<Integer> combinations = new ArrayList<Integer>();
                 combinations.add(i);
-                findNextCombinationDigits(pizzaArray.length,pizzaToBuy,combinations,pizzaArray,tempPizza);
+                findNextCombinationDigits(pizzaArray.length,pizzaToBuy,combinations,pizzaArray,tempPizza,maxSlice);
         }
-        System.out.println("Printing Combinations...");
-        //tempPizza.printPizzaCombinationList();
-
-        // for(List<Integer> temp : combinationList){
-        //     System.out.println(temp.toString());
-        // }
+        System.out.println("Min: "+minVal);
+        System.out.println("Max: "+maxVal);
+        System.out.println("Index: "+combinationIndex.toString());
+        List<Integer> tempCombinationIndex = new ArrayList<Integer>();
+        for(int index: combinationIndex){
+            tempCombinationIndex.add(index);
+        }
+        System.out.println(tempCombinationIndex.toString()+"tempCom main");
+        tempPizza.addPizza(minVal,maxVal,tempCombinationIndex);
+        }
+        System.out.println("Printing List..");
+        tempPizza.printList();
     }
 
-    public static void findNextCombinationDigits(int totalPizza, int maxCombination, List<Integer> combinations,int[] pizzaArray, MorePizza tempPizza){
+    public static void findNextCombinationDigits(int totalPizza, int maxCombination, List<Integer> combinations,int[] pizzaArray, MorePizza tempPizza, int maxSlice){
         if(combinations.size() == maxCombination){
             System.out.println(combinations.toString());
-            tempPizza.addPizzaCombination(combinations);
-            tempPizza.printPizzaCombinationList();
+            int sum = 0;
+            for(int index : combinations){
+                sum+= pizzaArray[index];
+            }
+            if(minVal==0){
+                minVal = sum;
+            }else{
+                if(minVal > sum){
+                    minVal = sum;
+                }
+            }
+            if(maxVal==0){
+                if(sum<maxSlice){
+                    maxVal = sum;
+                    combinationIndex = new ArrayList<Integer>();
+                    for(int i: combinations){
+                        combinationIndex.add(i);
+                    }
+                }
+            }else{
+                if(maxVal < sum && sum <= maxSlice){
+                    maxVal = sum;
+                    combinationIndex = new ArrayList<Integer>();
+                    for(int i: combinations){
+                        combinationIndex.add(i);
+                    }
+                }
+            }
+
+            //tempPizza.addPizzaCombination(combinations);
+            //tempPizza.printPizzaCombinationList();
+
             //combinationList.add(combinations);
             //System.out.println(combinationList.toString());
             //return combinations;
@@ -95,7 +138,7 @@ public class MorePizza {
             for(int k = lastCombinationDigit; k<totalPizza; k++){
                 if(k!=lastCombinationDigit){
                     combinations.add(k);
-                    findNextCombinationDigits(totalPizza, maxCombination, combinations,pizzaArray,tempPizza);
+                    findNextCombinationDigits(totalPizza, maxCombination, combinations,pizzaArray,tempPizza,maxSlice);
                     combinations.remove(combinations.size()-1);
                 }
             }
